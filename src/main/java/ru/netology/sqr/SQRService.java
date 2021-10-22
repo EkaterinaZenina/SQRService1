38cc8d7f82eee8b7bbc4d6f803e3db8304ae626d
Ekaterina.Zenina <zeninaekaterin@gmail.com>
2021-10-22 11:34:33 +0300

    Init

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public int sqr (int UnderNumber, int UpperNumber) {
+        int counter = 0;
+        for(int i = 10; i <= 99; i++){
+           int sqr = i * i; {
+                if (UnderNumber <= sqr);
+                if (UpperNumber >=sqr );
+                 int сounter = counter + 1;
+            }
+        }
+        return counter;
+        }
+    }
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQRServiceTest {
+
+    @Test
+    void shouldThreeSqr() {
+         SQRService service = new SQRService();
+         int UnderNumber = 200;
+         int UpperNumber = 300;
+         int expected = 3;
+
+         int actual = service.sqr(UpperNumber, UnderNumber);
+
+
+         assertEquals(expected, actual);
+    }
+    @Test
+    void shouldOneSqr() {
+        SQRService service = new SQRService();
+        int UnderNumber = 100;
+        int UpperNumber = 100;
+        int expected = 1;
+
+        int actual = service.sqr(UpperNumber, UnderNumber);
+
+
+        assertEquals(expected, actual);
+    }
+    
+}
