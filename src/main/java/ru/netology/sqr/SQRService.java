7596d03f7c336f9b86a8fddf060ea39b23b0e1b3
EkaterinaZenina <90308326+EkaterinaZenina@users.noreply.github.com>
2021-10-27 19:38:04 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,15 +2,14 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int sqr (int UnderNumber, int UpperNumber) {
+    public int sqr(int UnderNumber, int UpperNumber) {
         int counter = 0;
-        for(int i = 10; i <= 99; i++){
-           int sqr = i * i; {
-                if (UnderNumber <= sqr);
-                if (UpperNumber >=sqr );
-                 int сounter = counter + 1;
+        for (int i = 10; i <= 99; i++) {
+            int sqr = i * i;
+            if (sqr >= UpperNumber && sqr <= UnderNumber) {
+                int сounter = counter + 1;
             }
         }
         return counter;
-        }
     }
+}
